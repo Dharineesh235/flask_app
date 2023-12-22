4efa4e882d2b7f3ef77f6f0c5fdf3159b61a631f
Dharineesh235 <dharineesh235@gmail.com>
2023-12-22 17:01:03 +0530

    static login form added

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1', port=5000)
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# print("Flask App")
+
+# @app.route('/', methods=['GET', 'POST'])
+# def funk():
+#     return 'I am Flask sample application.'
+
+# if __name__ == "__main__":
+#     app.run(debug=True)    
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=8080)
